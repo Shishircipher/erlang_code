6b03798b1e08987676c30a87709ad50e120644c6
Shishir Chaurasiya <shishirchaurasiya17@gmail.com>
2025-03-01 06:17:10 +0000

    spawn ex1 added

---
+++ src/spawn_ex1.erl
@@ -0,0 +1,16 @@
+-module(spawn_ex1).
+
+-export([start/0, say_something/2]).
+
+say_something(What, 0) ->
+	done;
+
+%%% compile but compiler throw error - spawn_ex1.erl:5: Warning: variable 'What' is unused
+% spawn_ex1.erl:9: Warning: wrong number of arguments in format call
+say_something(What, Times) ->
+	io:format("p-n", [What]),
+	say_something(What, Times - 1).
+
+start() ->
+	spawn(spawn_ex1, say_something, [hello, 3]),
+	spawn(spawn_ex1, say_something, [goodbye, 3]).
